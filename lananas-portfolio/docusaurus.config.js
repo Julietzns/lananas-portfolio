cee8b98d916ccfb050bbffc1cf758043a6fdcbf0
Julie Thezenas <julie@lydra.fr>
2022-07-02 17:54:02 +0200

    deployment

--- lananas-portfolio/docusaurus.config.js
+++ lananas-portfolio/docusaurus.config.js
@@ -6,18 +6,18 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'Lananas Portfolio',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://julietzns.github.io',
+  baseUrl: '/lananas-portfolio/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Julietzns', // Usually your GitHub org/user name.
+  projectName: 'lananas-portfolio', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
