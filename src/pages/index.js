ece74f6f0847c2601ded5f7ac5c525e830ff83a2
Julie Thezenas <julie@lydra.fr>
2022-07-05 16:40:16 +0200

    feat: add title & img boxes

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import SmilingJu from "/static/img/smiling_ju.png";
-// import styles from "../css/main_header.scss";
+import styles from "../css/mainheader.css";
 
 
 function HomepageHeader() {
@@ -17,9 +17,19 @@ function HomepageHeader() {
         <p className="hero__subtitle">{siteConfig.tagline}</p> */}
         <div className="text-header">
           <h1 className="title-h1">Une Designeuse se perd dans le code</h1>
-          <p className="subtitle-p">Je vais faire un composant pour les textes</p>
+          <p className="subtitle-p">
+          Je me présente, Julie graphiste ayant fait une reconversion pro' dans le front-end. Durant nos modules nous avions touché à de l'UI-UX ce qui m'a redonné goût au design et depuis je m'auto-forme.
+          </p>
         </div>
-        <img src={SmilingJu} className="img-smiling-ju" alt="smiling-julie" />
+
+        <div className="block-right">
+          <img src={SmilingJu} className="img-smiling-ju" alt="smiling-julie" />
+          <p className="name-lastname">Julie Thézénas</p>
+          <p className="designer">UI UX designer</p>
+          <p className="developer">Front-end développeuse</p>
+          <div className="CV-box">CV à DL</div>
+        </div>
+        
       </div>
     </header>
   );
